469f2c86f7bb21332d09f197a9e32867b7d6bc61
Marlon Baldovino <37963450+sharkwhite21@users.noreply.github.com>
2021-03-28 13:56:38 -0500

    Interacciones, optimizacion codigo.

---
+++ JS/index.js
@@ -0,0 +1,4 @@
+let newRoute = document.querySelector("body > main > div > div:nth-child(1)");
+newRoute.addEventListener('click', function() {
+        window.location.pathname = '/companies.html' ;
+    })
--- JS/scritpCompanies.js
+++ JS/scritpCompanies.js
@@ -1,24 +1,11 @@
 let active = document.querySelector(".last-box");
 let cerrar = document.querySelector("body > main > div.zoom > div > img");
-active.addEventListener('click',modal,false);
-cerrar.addEventListener('click',close,false);
-
-function modal(e){
-    let modal = document.querySelector('body > main > div.zoom');
-
-    modal.removeAttribute('display');
-    modal.style.display= "flex";
-
-    active.removeAttribute('display');
-    active.style.display= "none";
-}
-
-function close(e) {
-    let modal = document.querySelector('body > main > div.zoom');
-
-    modal.removeAttribute('display');
-    modal.style.display= "none";
-
-    active.removeAttribute('display');
-    active.style.display= "flex";
-}
+let zoom =document.querySelector("body > main > div.zoom ")
+active.addEventListener('click', function() {
+    zoom.style.display = "flex";
+    active.style.display = 'none'
+})
+cerrar.addEventListener('click', function() {
+    zoom.style.display = "none";
+    active.style.display = 'flex'
+})
